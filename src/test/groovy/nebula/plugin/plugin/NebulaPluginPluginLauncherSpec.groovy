7a03056e980f99ac45e82dce66c244b18b84572c
Benjamin Muschko <benjamin.muschko@gmail.com>
2014-07-11 11:37:19 -0700

    Integration test SourceSet should configure IDEA plugin if applied.

--- src/test/groovy/nebula/plugin/plugin/NebulaPluginPluginLauncherSpec.groovy
+++ src/test/groovy/nebula/plugin/plugin/NebulaPluginPluginLauncherSpec.groovy
@@ -1,8 +1,8 @@
 package nebula.plugin.plugin
 
-import java.util.jar.JarEntry
-import java.util.jar.JarFile
 import nebula.test.IntegrationSpec
+
+import java.util.jar.JarFile
 
 class NebulaPluginPluginLauncherSpec extends IntegrationSpec {
 
@@ -55,4 +55,54 @@ class NebulaPluginPluginLauncherSpec extends IntegrationSpec {
         files.contains 'META-INF/gradle-plugins/pluginone.properties'
     }
 
+    def "Can run integration tests with sample test"() {
+        when:
+        buildFile << """
+apply plugin: 'nebula-plugin'
+
+dependencies {
+    integrationTestCompile 'junit:junit:4.8.2'
+}
+"""
+
+        writeHelloWorld('nebula.plugin.plugin')
+        writeTest('src/integrationTest/java/', 'nebula.plugin.plugin', false)
+        runTasksSuccessfully('integrationTest')
+
+        then:
+        File integrationTestClassFile = new File(projectDir, 'build/classes/integrationTest/nebula/plugin/plugin/HelloWorldTest.class')
+        integrationTestClassFile.exists()
+        File integrationTestTestResultsFile = new File(projectDir, 'build/test-results/TEST-nebula.plugin.plugin.HelloWorldTest.xml')
+        integrationTestTestResultsFile.exists()
+    }
+
+    def "Configures Idea project files for created integration test SourceSet and task"() {
+        when:
+        buildFile << """
+apply plugin: 'nebula-plugin'
+apply plugin: 'idea'
+
+dependencies {
+    integrationTestCompile 'junit:junit:4.8.2'
+    integrationTestRuntime 'mysql:mysql-connector-java:5.1.27'
+}
+"""
+
+        writeHelloWorld('nebula.plugin.plugin')
+        writeTest('src/integrationTest/java/', 'nebula.plugin.plugin', false)
+        runTasksSuccessfully('idea')
+
+        then:
+        File ideaModuleFile = new File(projectDir, "${moduleName}.iml")
+        ideaModuleFile.exists()
+        def moduleXml = new XmlSlurper().parseText(ideaModuleFile.text)
+        def testSourceFolders = moduleXml.component.content.sourceFolder.findAll { it.@isTestSource.text() == 'true' }
+        def testSourceFolder = testSourceFolders.find { it.@url.text() == "file://\$MODULE_DIR\$/src/integrationTest/java" }
+        testSourceFolder
+        def orderEntries = moduleXml.component.orderEntry.findAll { it.@type.text() == 'module-library' && it.@scope.text() == 'TEST' }
+        def junitLibrary = orderEntries.find { it.library.CLASSES.root.@url.text().contains('junit-4.8.2.jar') }
+        junitLibrary
+        def mysqlLibrary = orderEntries.find { it.library.CLASSES.root.@url.text().contains('mysql-connector-java-5.1.27.jar') }
+        mysqlLibrary
+    }
 }
