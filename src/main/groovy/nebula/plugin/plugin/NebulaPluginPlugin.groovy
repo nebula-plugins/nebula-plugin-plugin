fef624687f70ba8498f559422099b8813b5a1bbf
Danny Thomas <dannyt@netflix.com>
2016-05-30 14:15:09 -0700

    Add dependency lock plugin

--- src/main/groovy/nebula/plugin/plugin/NebulaPluginPlugin.groovy
+++ src/main/groovy/nebula/plugin/plugin/NebulaPluginPlugin.groovy
@@ -31,6 +31,7 @@ class NebulaPluginPlugin implements Plugin<Project> {
     static final THIRDPARTY_PLUGIN_IDS = ['com.github.kt3k.coveralls']
 
     static final NEBULA_PLUGIN_IDS = ['nebula.contacts',
+                                      'nebula.dependency-lock',
                                       'nebula.facet',
                                       'nebula.info',
                                       'nebula.javadoc-jar',
