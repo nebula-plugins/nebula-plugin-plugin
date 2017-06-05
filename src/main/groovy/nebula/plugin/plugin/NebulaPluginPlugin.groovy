bc77f39f0791f0fb7e07a74768fb167d68bdfde6
Rob Spieldenner <rob.spieldenner@gmail.com>
2017-06-05 16:16:46 -0700

    Try to publish resolved versions

--- src/main/groovy/nebula/plugin/plugin/NebulaPluginPlugin.groovy
+++ src/main/groovy/nebula/plugin/plugin/NebulaPluginPlugin.groovy
@@ -15,8 +15,11 @@
  */
 package nebula.plugin.plugin
 
+import org.apache.maven.model.Dependency
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.component.ModuleComponentSelector
+import org.gradle.api.artifacts.result.ResolvedDependencyResult
 import org.gradle.api.tasks.testing.Test
 /**
  * Provide an environment for a Gradle plugin
@@ -106,6 +109,8 @@ class NebulaPluginPlugin implements Plugin<Project> {
                 tasks.publishPlugins.dependsOn tasks.check
                 tasks.bintrayUpload.dependsOn tasks.publishPlugins
 
+                enableResolvedVersionInPluginPortalPom(project)
+
                 gradle.taskGraph.whenReady { graph ->
                     tasks.publishPlugins.onlyIf {
                         graph.hasTask(':final')
@@ -114,4 +119,24 @@ class NebulaPluginPlugin implements Plugin<Project> {
             }
         }
     }
+
+    def enableResolvedVersionInPluginPortalPom(Project project) {
+        project.pluginBundle {
+            def resolvedDeps = project.configurations.runtimeClasspath.incoming.resolutionResult.allDependencies
+            withDependencies { List<Dependency> deps ->
+                deps.each { Dependency dep ->
+                    String group = dep.groupId
+                    String artifact = dep.artifactId
+                    ResolvedDependencyResult found = resolvedDeps.find { r ->
+                        (r.requested instanceof ModuleComponentSelector) &&
+                                (r.requested.group == group) &&
+                                (r.requested.module == artifact)
+                    }
+
+                    dep.version = found.selected.moduleVersion.version
+                }
+            }
+        }
+    }
+
 }
